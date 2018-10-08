d28b7c006236c01a012120121f09787cd65f1e32
elisoft2018 <johneroblesa@gmail.com>
2018-10-08 17:53:55 -0500

    calculadora

--- JS/Person.js
+++ JS/Person.js
@@ -10,13 +10,17 @@ function bornYear() {
 persona=new Person("jose",34);
 console.log("El Sr. "+persona.name+" naciò en "+persona.yearOfBirth()+" y tiene "+persona.age+" años de edad");*/
 
+var operando1 = parseInt(prompt("digite numero uno")); //document.calc.operando1.value
+var operando2 = parseInt(prompt("digite numero 2")); //document.calc.operando2.value 
+var operacion = prompt("digite la operacion que desea realizar +,-,*,/");
 
-function calcula(operacion){
-    var operando1 = document.calc.operando1.value
-    var operando2 = document.calc.operando2.value 
-    var result = eval(operando1 + operacion + operando2)
-    document.calc.resultado.value = result
+function calcula(operando1, operacion, operando2) {
+    var result = eval(operando1 + operacion + operando2);
+    return result;
 }
+
+alert(calcula(this.result));
+
 
 /*var pos=0;
 var box= document.getElementById("sobrepuesto");
